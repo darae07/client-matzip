96b7e71d8b72cea29bf0ffd4adbd6eaf81ca84f9
darae07 <leedr0730@gmail.com>
2022-02-17 23:46:09 +0900

    http 상수

---
+++ constants/httpRequest.ts
@@ -0,0 +1,6 @@
+export default {
+  REQUEST_HEADER_CONTENTS_JSON: 'application/json',
+  REQUEST_HEADER_CONTENTS_MULTIPART_FORM: 'multipart/form-data',
+  // SERVICE_BASE_URL:'http://localhost:8000/', //LOCAL
+  SERVICE_BASE_URL: 'https://our-matzip.herokuapp.com/', //PRODUCTION
+}
