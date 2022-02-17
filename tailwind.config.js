53a6854d07e19678b27e39e954fcf153fb01784a
darae07 <leedr0730@gmail.com>
2022-02-17 23:37:48 +0900

    필요 패키지 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
