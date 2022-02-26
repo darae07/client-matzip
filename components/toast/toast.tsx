b0d70847a028d958fb2a6e7aa18425a30526f5ee
darae07 <leedr0730@gmail.com>
2022-02-26 19:29:59 +0900

    toast ui 변경

--- components/toast/toast.tsx
+++ components/toast/toast.tsx
@@ -27,29 +27,10 @@ export const Toast: FC = () => {
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 z-10 overflow-y-auto"
-        onClose={handleCloseToast}
+        className="fixed inset-0 z-30 overflow-y-auto"
+        onClose={() => {}}
       >
         <div className="min-h-screen  px-4 text-center">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0" />
-          </Transition.Child>
-
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className="inline-block h-screen align-middle"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -59,8 +40,8 @@ export const Toast: FC = () => {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="my-8 inline-block w-3/4 max-w-sm transform overflow-hidden rounded bg-gray-900  py-2 px-4 text-left opacity-80 shadow-xl transition-all">
-              <p className="text-white">test{message}</p>
+            <div className="my-8 inline-block w-3/4  max-w-sm translate-y-0 transform overflow-hidden rounded bg-gray-900  py-2 px-4 text-left opacity-80 shadow-xl transition-all">
+              <p className="text-white">{message}</p>
             </div>
           </Transition.Child>
         </div>
