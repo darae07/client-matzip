8c8079c4a9b9adf05c53a7e827b1a98ac54cb23e
darae07 <leedr0730@gmail.com>
2022-02-26 17:02:29 +0900

    로그인 액션 시작 추가

--- store/modules/auth/user.ts
+++ store/modules/auth/user.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { User } from 'type/user'
 
 interface UserState {
+  isLoading: boolean
   user: User | null
   errorMessage: string | null
 }
 const initialState: UserState = {
+  isLoading: false,
   user: null,
   errorMessage: null,
 }
@@ -14,20 +16,27 @@ export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    userLoginStart: (state) => {
+      state.isLoading = true
+    },
     userLogin: (state, action: PayloadAction<User>) => {
       state.user = action.payload
       state.errorMessage = null
+      state.isLoading = false
     },
     userLogout: (state) => {
       state.user = null
       state.errorMessage = null
+      state.isLoading = false
     },
     catchError: (state, action: PayloadAction<string>) => {
       state.user = null
       state.errorMessage = action.payload
+      state.isLoading = false
     },
   },
 })
 
-export const { userLogin, userLogout, catchError } = userSlice.actions
+export const { userLogin, userLogout, catchError, userLoginStart } =
+  userSlice.actions
 export default userSlice.reducer
