7d8b316dcac7026ee8e3972357a737f9c44019b8
darae07 <leedr0730@gmail.com>
2022-02-26 19:43:55 +0900

    svgr 설치 및 웹팩 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
