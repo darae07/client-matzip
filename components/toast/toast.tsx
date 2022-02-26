e17a6c327ed3a7c66d8dad87fa8eb6a4b553a763
darae07 <leedr0730@gmail.com>
2022-02-26 22:22:49 +0900

    toast initial focus 추가

--- components/toast/toast.tsx
+++ components/toast/toast.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment, useEffect, useState } from 'react'
+import React, { FC, Fragment, useEffect, useRef } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { useAppDispatch, useAppSelector } from 'hooks'
 import { closeToast } from 'store/modules/ui/toast'
@@ -18,10 +18,12 @@ export const Toast: FC = () => {
   useEffect(() => {
     if (isOpen) {
       setTimeout(() => {
-        handleCloseToast()
+        // handleCloseToast()
       }, disappearTime)
     }
   }, [isOpen])
+
+  const toastRef = useRef(null)
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
@@ -29,6 +31,7 @@ export const Toast: FC = () => {
         as="div"
         className="fixed inset-0 z-30 overflow-y-auto"
         onClose={() => {}}
+        initialFocus={toastRef}
       >
         <div className="min-h-screen  px-4 text-center">
           <Transition.Child
@@ -41,7 +44,9 @@ export const Toast: FC = () => {
             leaveTo="opacity-0 scale-95"
           >
             <div className="my-8 inline-block w-3/4  max-w-sm translate-y-0 transform overflow-hidden rounded bg-gray-900  py-2 px-4 text-left opacity-80 shadow-xl transition-all">
-              <p className="text-white">{message}</p>
+              <p className="text-white" ref={toastRef}>
+                {message}
+              </p>
             </div>
           </Transition.Child>
         </div>
