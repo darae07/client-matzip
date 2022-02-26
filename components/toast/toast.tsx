87105598b43f497ccc8c344de894dd6e830c82df
darae07 <leedr0730@gmail.com>
2022-02-26 18:07:44 +0900

    toast 컴포넌트

---
+++ components/toast/toast.tsx
@@ -0,0 +1,70 @@
+import React, { FC, Fragment, useEffect, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { useAppDispatch, useAppSelector } from 'hooks'
+import { closeToast } from 'store/modules/ui/toast'
+
+export const Toast: FC = () => {
+  const { isOpen, message, disappearTime } = useAppSelector((state) => ({
+    isOpen: state.toast.isOpen,
+    message: state.toast.message,
+    disappearTime: state.toast.disappearTime,
+  }))
+
+  const dispatch = useAppDispatch()
+  const handleCloseToast = () => {
+    dispatch(closeToast())
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        handleCloseToast()
+      }, disappearTime)
+    }
+  }, [isOpen])
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={handleCloseToast}
+      >
+        <div className="min-h-screen  px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="my-8 inline-block w-3/4 max-w-sm transform overflow-hidden rounded bg-gray-900  py-2 px-4 text-left opacity-80 shadow-xl transition-all">
+              <p className="text-white">test{message}</p>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
