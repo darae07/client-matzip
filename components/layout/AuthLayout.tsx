1a30699d6c35d7b69645eeaeed6df5603ffdb438
darae07 <leedr0730@gmail.com>
2022-02-27 22:43:15 +0900

    auth 레이아웃 정리

--- components/layout/AuthLayout.tsx
+++ components/layout/AuthLayout.tsx
@@ -1,12 +1,32 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { LayoutProps } from 'type/ui'
+import lottie from 'lottie-web'
 
 export default function AuthLayout({ children }: LayoutProps) {
+  const animation = useRef()
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: animation.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('public/animation/41570-time-relax-lunch.json'),
+    })
+  }, [])
   return (
-    <div className="flex h-screen w-screen items-center justify-self-center">
-      <div className="w-full max-w-[28rem] items-center p-10">{children}</div>
-      <div className="hidden items-center p-28 md:flex md:w-1/2">
-        이미지 영역
+    <div className="mx-auto flex h-screen w-fit items-center justify-self-center">
+      <div className="w-full max-w-[28rem] items-center p-10">
+        <div className="mx-auto">
+          <h1>오늘뭐먹지?</h1>
+          <h1 className="mb-4 text-3xl font-bold">
+            쉽고 빠른 점심메뉴 의사결정
+          </h1>
+          {children}
+        </div>
+      </div>
+
+      <div className="hidden w-full max-w-[35rem] items-center  md:flex">
+        <div ref={animation}></div>
       </div>
     </div>
   )
--- pages/login.tsx
+++ pages/login.tsx
@@ -23,50 +23,47 @@ const Login: NextPageWithLayout = () => {
 
   return (
     <div>
-      <div className="mx-auto">
-        <h1 className="text-3xl font-bold">쉽고 빠른 점심메뉴 의사결정</h1>
+      <p className="mb-4 text-slate-500 md:text-sm">
+        로그인 정보를 입력해주세요.
+      </p>
+
+      <LoginForm />
+      <div className="my-5 w-full border-t border-gray-300">
         <p className="my-4 text-slate-500 md:text-sm">
-          로그인 정보를 입력해주세요.
+          소셜 계정으로 3초만에 로그인/회원가입 하기
         </p>
-
-        <LoginForm />
-        <div className="my-5 w-full border-t border-gray-300">
-          <p className="my-4 text-slate-500 md:text-sm">
-            소셜 계정으로 3초만에 로그인/회원가입 하기
-          </p>
-        </div>
-        <div className="flex space-x-2">
-          {GOOGLE_KEY && (
-            <GoogleLogin
-              clientId={GOOGLE_KEY}
-              buttonText="로그인"
-              onSuccess={handleGoogleLoginSuccess}
-              className="flex w-1/2"
-            />
-          )}
-
-          {KAKAO_KEY && (
-            <KakaoLogin
-              token={KAKAO_KEY}
-              onSuccess={handleKakaoLoginSuccess}
-              onFail={handleKakaoLoginFail}
-              onLogout={handleKakaoLogout}
-              scopes={['id', 'kakao_account']}
-              render={({ onClick }) => (
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault()
-                    onClick()
-                  }}
-                  className="flex w-1/2 items-center justify-center rounded-md bg-[url(//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg)] py-2 px-2 font-semibold text-amber-900 md:text-sm"
-                />
-              )}
-            />
-          )}
-        </div>
-        <div></div>
       </div>
+      <div className="flex space-x-2">
+        {GOOGLE_KEY && (
+          <GoogleLogin
+            clientId={GOOGLE_KEY}
+            buttonText="로그인"
+            onSuccess={handleGoogleLoginSuccess}
+            className="flex w-1/2"
+          />
+        )}
+
+        {KAKAO_KEY && (
+          <KakaoLogin
+            token={KAKAO_KEY}
+            onSuccess={handleKakaoLoginSuccess}
+            onFail={handleKakaoLoginFail}
+            onLogout={handleKakaoLogout}
+            scopes={['id', 'kakao_account']}
+            render={({ onClick }) => (
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault()
+                  onClick()
+                }}
+                className="flex w-1/2 items-center justify-center rounded-md bg-[url(//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg)] py-2 px-2 font-semibold text-amber-900 md:text-sm"
+              />
+            )}
+          />
+        )}
+      </div>
+      <div></div>
     </div>
   )
 }
