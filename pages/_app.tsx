3812ce8ef924caa6b47e44775760bd2329f0b534
darae07 <leedr0730@gmail.com>
2022-02-27 22:20:15 +0900

    layout 페이지 타입에 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,28 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider, ReactReduxContext, useStore } from 'react-redux'
+import { useStore } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { wrapper } from 'store'
-import App from 'next/app'
 import { persistStore } from 'redux-persist'
 import { injectStore } from 'api'
 import { Toast } from 'components/toast/toast'
-import Script from 'next/script'
-import { useEffect } from 'react'
+import { AppPropsWithLayout } from 'type/ui'
+import { ReactNode } from 'react'
 
-export default wrapper.withRedux(({ Component, pageProps }: AppProps) => {
-  const store = useStore()
-  const persistor = persistStore(store)
-  injectStore(store)
+export default wrapper.withRedux(
+  ({ Component, pageProps }: AppPropsWithLayout) => {
+    const store = useStore()
+    const persistor = persistStore(store)
+    injectStore(store)
 
-  return (
-    <>
-      <PersistGate persistor={persistor} loading={null}>
-        <Toast />
-        <Component {...pageProps} />
-      </PersistGate>
-    </>
-  )
-})
+    const getLayout = Component.getLayout ?? ((page: ReactNode) => page)
+
+    return (
+      <>
+        <PersistGate persistor={persistor} loading={null}>
+          <Toast />
+          {getLayout(<Component {...pageProps} />)}
+        </PersistGate>
+      </>
+    )
+  },
+)
---
+++ type/ui.ts
@@ -0,0 +1,15 @@
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+
+export interface LayoutProps {
+  children: React.ReactNode
+}
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
