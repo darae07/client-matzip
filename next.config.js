8663d43255bc3013db09c17fe750643f85e34595
darae07 <leedr0730@gmail.com>
2022-02-28 21:16:29 +0900

    image 도메인 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
