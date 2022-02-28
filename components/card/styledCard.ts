6b8f508906a397676f1fdae7fa04768e0e9d24ca
darae07 <leedr0730@gmail.com>
2022-03-01 02:36:56 +0900

    스타일 수정

---
+++ components/card/styledCard.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+export const WhiteRoundedCard = styled.div`
+  ${tw`rounded-lg bg-white py-6 px-7 text-gray-800 drop-shadow-sm`}
+`
