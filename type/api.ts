cfe91b71ae0dce08765225322b344f55aaea951b
darae07 <leedr0730@gmail.com>
2022-03-04 00:31:13 +0900

    api 응답 타입설정

--- type/api.ts
+++ type/api.ts
@@ -1,5 +1,15 @@
-export interface ApiResponseType {
+import { AxiosResponse, AxiosError } from 'axios'
+
+export interface ApiResponse extends AxiosResponse {
+  data: ApiResponseData
+}
+
+export interface ApiResponseData {
   result?: any
   message: null | string
   success: boolean
+}
+
+export interface ApiErrorResponse extends AxiosError {
+  response: ApiResponse
 }
