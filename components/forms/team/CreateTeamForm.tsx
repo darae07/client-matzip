49db4eae8ab50b59693a672c620e2c527ba45dca
darae07 <leedr0730@gmail.com>
2022-03-04 00:31:35 +0900

    팀생성 폼 작성

--- components/forms/team/CreateTeamForm.tsx
+++ components/forms/team/CreateTeamForm.tsx
@@ -3,11 +3,14 @@ import { Field, Formik, Form } from 'formik'
 import { Input } from 'components'
 import * as Yup from 'yup'
 import { locationDongReg } from 'constants/validation'
-
-interface TeamValue {
-  name: string
-  location: string
-}
+import { CreateTeamValue } from 'type/team'
+import { QueryClient, useMutation, useQueryClient } from 'react-query'
+import { createTeam } from 'api/team'
+import { ApiResponseData, ApiErrorResponse } from 'type/api'
+import { openToast } from 'store/modules/ui/toast'
+import { useAppDispatch } from 'hooks'
+import { setUserTeamProfile } from 'store/modules/auth/user'
+import _ from 'lodash'
 
 const teamValues = {
   name: '',
@@ -27,7 +30,27 @@ export const CreateTeamForm: FC = () => {
       .matches(locationDongReg, '위치를 동단위로 입력해 주세요'),
   })
 
-  const handleCreateTeam = (values: TeamValue) => {}
+  const handleCreateTeam = (values: CreateTeamValue) => {
+    mutate(values)
+  }
+
+  const dispatch = useAppDispatch()
+  const queryClient = useQueryClient()
+
+  const { mutate, isLoading } = useMutation(createTeam, {
+    onSuccess: (data: ApiResponseData) => {
+      const { message, result } = data
+      dispatch(openToast(message || '팀을 생성했습니다.'))
+      queryClient.setQueriesData('myTeam', result)
+      dispatch(setUserTeamProfile(result.team_profile))
+    },
+    onError: (error: ApiErrorResponse) => {
+      const { message } = error.response.data
+      dispatch(
+        openToast(_.isString(message) ? message : '팀을 생성할 수 없습니다.'),
+      )
+    },
+  })
 
   return (
     <div>
@@ -35,7 +58,7 @@ export const CreateTeamForm: FC = () => {
         enableReinitialize={true}
         initialValues={teamValues}
         validationSchema={teamFormSchema}
-        onSubmit={handleCreateTeam}
+        onSubmit={(values) => handleCreateTeam(values)}
       >
         {({ handleSubmit, values }) => (
           <Form>
@@ -55,6 +78,7 @@ export const CreateTeamForm: FC = () => {
             <div className="mb-2.5"></div>
             <button
               type="submit"
+              disabled={isLoading}
               onSubmit={() => handleSubmit()}
               className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
             >
