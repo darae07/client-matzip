5b5aad3042be4f7fb08e3ae7776e3e64a0713637
darae07 <leedr0730@gmail.com>
2022-03-05 22:19:45 +0900

    팀 정보 표시

--- components/modules/team/TeamInformation.tsx
+++ components/modules/team/TeamInformation.tsx
@@ -1,4 +1,5 @@
 import { retrieveTeam } from 'api/team'
+import { WhiteRoundedCard } from 'components/card/styledCard'
 import { useAppSelector } from 'hooks'
 import { FC, useEffect } from 'react'
 import { useMutation, useQuery } from 'react-query'
@@ -9,10 +10,22 @@ const TeamInformation: FC = () => {
   const teamId = team_profile?.team
 
   useEffect(() => {})
-  const myTeam = useQuery(['myTeam', teamId], () => retrieveTeam(teamId), {
-    enabled: !!teamId,
-  })
-  return <div></div>
+  const { data, error, isLoading } = useQuery(
+    ['myTeam', teamId],
+    () => retrieveTeam(teamId),
+    {
+      enabled: !!teamId,
+    },
+  )
+
+  return (
+    <WhiteRoundedCard>
+      <div className="mb-2">회사 정보</div>
+      <div className="font-bold">{data?.name}</div>
+      <div>입장 코드: {data?.join_code}</div>
+      <div>위치: {data?.location}</div>
+    </WhiteRoundedCard>
+  )
 }
 
 export { TeamInformation }
