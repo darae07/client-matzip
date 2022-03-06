0fdebefa0a99a5a53c6ddedf4e9d79f2ef199a83
darae07 <leedr0730@gmail.com>
2022-03-06 21:15:17 +0900

    팀정보 여부에따라 보이기

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router'
 import CreateTeamModal from 'components/modal/team/CreateTeamModal'
 import { TeamInformation } from 'components/modules/team/TeamInformation'
 import FindTeamModal from 'components/modal/team/FindTeamModal'
+import { useAppSelector } from 'hooks'
 
 const TeamPage: NextPageWithLayout = () => {
   const router = useRouter()
@@ -15,28 +16,42 @@ const TeamPage: NextPageWithLayout = () => {
   const showTeamCreateModal = asPath === '/team/create'
   const showTeamFindModal = asPath === '/team/find'
 
+  const user = useAppSelector((state) => state.user)
+  const team_profile = user.user?.team_profile
+
   return (
     <div>
-      <WhiteRoundedCard className="mb-4 cursor-pointer">
-        <Link href="/team" as="/team/create">
-          <div>
-            <span className="mr-2 text-3xl">👋 </span>서비스가 처음이신가요?
-            회사를 등록하고 동료들과 함께 점심을 즐기세요
+      {!team_profile && (
+        <>
+          <WhiteRoundedCard className="mb-4 cursor-pointer">
+            <Link href="/team" as="/team/create">
+              <div>
+                <span className="mr-2 text-3xl">👋 </span>서비스가 처음이신가요?
+                회사를 등록하고 동료들과 함께 점심을 즐기세요
+              </div>
+            </Link>
+          </WhiteRoundedCard>
+          <WhiteRoundedCard className="mb-4 cursor-pointer">
+            <Link href="/team" as="/team/find">
+              <div>
+                <span className="mr-2 text-3xl">🤝</span> 동료에게 입장 코드를
+                공유받으셨나요? 입장 코드로 회사에 합류해 보세요
+              </div>
+            </Link>
+          </WhiteRoundedCard>
+        </>
+      )}
+      {!!team_profile && (
+        <>
+          <div className="mb-4">
+            <TeamInformation />
           </div>
-        </Link>
-      </WhiteRoundedCard>
-      <WhiteRoundedCard className="mb-4 cursor-pointer">
-        <Link href="/team" as="/team/find">
-          <div>
-            <span className="mr-2 text-3xl">🤝</span> 동료에게 입장 코드를
-            공유받으셨나요? 입장 코드로 회사에 합류해 보세요
-          </div>
-        </Link>
-      </WhiteRoundedCard>
-      <div className="mb-4">
-        <TeamInformation />
-      </div>
-      <WhiteRoundedCard className="mb-4">우리 회사 주변 맛집</WhiteRoundedCard>
+          <WhiteRoundedCard className="mb-4">
+            우리 회사 주변 맛집
+          </WhiteRoundedCard>
+        </>
+      )}
+
       {showTeamCreateModal && <CreateTeamModal />}
       {showTeamFindModal && <FindTeamModal />}
     </div>
