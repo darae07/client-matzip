698dc6f531a49824454bbc091c9be5df9d7eec79
darae07 <leedr0730@gmail.com>
2022-03-10 00:32:20 +0900

    axios 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,24 @@ import { useStore } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { wrapper } from 'store'
 import { persistStore } from 'redux-persist'
-import { injectStore } from 'api'
+import { injectStore, setupAxiosInterceptors } from 'api'
 import { Toast } from 'components/toast/toast'
 import { AppPropsWithLayout } from 'type/ui'
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { useRouter } from 'next/router'
 
 export default wrapper.withRedux(
   ({ Component, pageProps }: AppPropsWithLayout) => {
     const store = useStore()
     const persistor = persistStore(store)
+    const router = useRouter()
+
     injectStore(store)
+    useEffect(() => {
+      setupAxiosInterceptors(store, router)
+    }, [])
 
     const getLayout = Component.getLayout ?? ((page: ReactNode) => page)
     const queryClient = new QueryClient()
