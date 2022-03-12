04d3f697ff9b9fc05797662e941bec9977ba6516
darae07 <leedr0730@gmail.com>
2022-03-13 01:08:18 +0900

    api 응답 타입 변수 적용

--- api/party/read.ts
+++ api/party/read.ts
@@ -2,16 +2,30 @@ import { authorizedInstance } from 'api/setupAxios'
 import { ApiResponse } from 'type/api'
 import { isValidId } from 'type/validation'
 
-export const listParty = async () => {
-  const { data: response }: ApiResponse = await authorizedInstance.get(
+export const listParty = async <ResultT>() => {
+  const { data: response }: ApiResponse<ResultT> = await authorizedInstance.get(
     'group/party/',
   )
   return response.result
 }
 
-export const listMyParty = async () => {
-  const { data: response }: ApiResponse = await authorizedInstance.get(
+export const listMyParty = async <ResultT>() => {
+  const { data: response }: ApiResponse<ResultT> = await authorizedInstance.get(
     'group/party/my_party/',
   )
   return response.result
+}
+
+export const retrieveParty = async <ResultT>(
+  id: string | string[] | undefined,
+) => {
+  try {
+    return await isValidId(id)
+  } catch (e) {
+    console.log(e)
+  } finally {
+    const { data: response }: ApiResponse<ResultT> =
+      await authorizedInstance.get(`group/party/${id}/`)
+    return response.result
+  }
 }
--- api/team/find.ts
+++ api/team/find.ts
@@ -2,10 +2,8 @@ import { authorizedInstance } from 'api/setupAxios'
 import { ApiResponse } from 'type/api'
 import { FindTeamValue } from 'type/team'
 
-export const findTeamByCode = async (data: FindTeamValue) => {
-  const { data: response }: ApiResponse = await authorizedInstance.post(
-    '/group/team/find/',
-    data,
-  )
+export const findTeamByCode = async <ResultT>(data: FindTeamValue) => {
+  const { data: response }: ApiResponse<ResultT> =
+    await authorizedInstance.post('/group/team/find/', data)
   return response
 }
