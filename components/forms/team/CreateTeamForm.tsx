698f55d7adb9026bf831313dcfaa4945d4575aaa
darae07 <leedr0730@gmail.com>
2022-03-13 23:24:23 +0900

    팀생성폼 수정

--- components/forms/team/CreateTeamForm.tsx
+++ components/forms/team/CreateTeamForm.tsx
@@ -3,14 +3,15 @@ import { Field, Formik, Form } from 'formik'
 import { Input } from 'components'
 import * as Yup from 'yup'
 import { locationDongReg } from 'constants/validation'
-import { CreateTeamValue } from 'type/team'
-import { useMutation, useQueryClient } from 'react-query'
+import { CreateTeam, CreateTeamValue } from 'type/team'
+import { useQueryClient } from 'react-query'
 import { createTeam } from 'api/team'
-import { ApiResponseData, ApiErrorResponse } from 'type/api'
+import { ApiResponseData } from 'type/api'
 import { openToast } from 'store/modules/ui/toast'
 import { useAppDispatch } from 'hooks'
 import { setUserTeamProfile } from 'store/modules/auth/user'
 import _ from 'lodash'
+import useMutationHandleError from 'hooks/useMutationHandleError'
 
 const teamValues: CreateTeamValue = {
   name: '',
@@ -30,27 +31,25 @@ export const CreateTeamForm: FC = () => {
       .matches(locationDongReg, '위치를 동단위로 입력해 주세요'),
   })
 
-  // const handleCreateTeam = (values: CreateTeamValue) => {
-  //   mutate(values)
-  // }
+  const handleCreateTeam = (values: CreateTeamValue) => {
+    mutate(values)
+  }
 
-  // const dispatch = useAppDispatch()
-  // const queryClient = useQueryClient()
+  const dispatch = useAppDispatch()
+  const queryClient = useQueryClient()
 
-  // const { mutate, isLoading } = useMutation(createTeam, {
-  //   onSuccess: (data: ApiResponseData) => {
-  //     const { message, result } = data
-  //     dispatch(openToast(message || '회사를 생성했습니다.'))
-  //     queryClient.setQueryData(['myTeam'], result)
-  //     dispatch(setUserTeamProfile(result.team_profile))
-  //   },
-  //   onError: (error: ApiErrorResponse) => {
-  //     const { message } = error.response.data
-  //     dispatch(
-  //       openToast(_.isString(message) ? message : '회사를 생성할 수 없습니다.'),
-  //     )
-  //   },
-  // })
+  const { mutate, isLoading } = useMutationHandleError(
+    createTeam,
+    {
+      onSuccess: (data: ApiResponseData<CreateTeam>) => {
+        const { message, result } = data
+        dispatch(openToast(message || '회사를 생성했습니다.'))
+        queryClient.setQueryData(['myTeam'], result)
+        dispatch(setUserTeamProfile(result.team_profile))
+      },
+    },
+    '회사를 생성할 수 없습니다.',
+  )
 
   return (
     <div>
@@ -58,7 +57,7 @@ export const CreateTeamForm: FC = () => {
         enableReinitialize={true}
         initialValues={teamValues}
         validationSchema={teamFormSchema}
-        onSubmit={(values) => {}}
+        onSubmit={(values) => handleCreateTeam(values)}
       >
         {({ handleSubmit, values }) => (
           <Form>
@@ -78,7 +77,7 @@ export const CreateTeamForm: FC = () => {
             <div className="mb-2.5"></div>
             <button
               type="submit"
-              // disabled={isLoading}
+              disabled={isLoading}
               onSubmit={() => handleSubmit()}
               className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
             >
