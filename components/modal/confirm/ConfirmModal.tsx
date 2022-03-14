6e9e96a428c345e2eb6cf2292dabd5d857f6775d
darae07 <leedr0730@gmail.com>
2022-03-14 14:49:50 +0900

    컨펌모달

--- components/modal/confirm/ConfirmModal.tsx
+++ components/modal/confirm/ConfirmModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
 import { XIcon } from '@heroicons/react/outline'
@@ -16,48 +16,58 @@ export const ConfirmModal: React.FC<ModalProps> = ({
   message,
   options,
 }) => {
-  const closeModal = () => {}
+  const [isOpen, setIsOpen] = useState(true)
 
-  return (
-    <Transition appear show={true} as={Fragment}>
-      <Dialog
-        as="div"
-        className="fixed inset-0 z-[25] overflow-y-auto "
-        onClose={closeModal}
-      >
-        <div className="min-h-screen px-4 text-center">
-          <ModalTransition>
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-50 transition-opacity" />
-          </ModalTransition>
+  const onConfirm = () => {
+    setIsOpen(false)
+    resolver(true)
+  }
+  const onCancel = () => {
+    setIsOpen(false)
+    resolver(false)
+  }
 
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className="inline-block h-screen align-middle"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
-          <ModalTransition>
-            <div className="my-8 inline-block w-full max-w-screen-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+  return isOpen ? (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+        <div className="relative my-6 mx-auto w-auto min-w-[50%] max-w-3xl">
+          {/*content*/}
+          <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
+            {/*header*/}
+            <div className="border-blueGray-200 flex items-start justify-between rounded-t  p-5">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
+                {message}
+              </h3>
               <button
-                type="button"
-                className="absolute right-6 text-gray-400"
-                onClick={closeModal}
+                className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-gray-400 outline-none focus:outline-none"
+                onClick={onCancel}
               >
                 <XIcon className="h-6 w-6" />
               </button>
-              <Dialog.Title
-                as="h3"
-                className="text-lg font-medium leading-6 text-gray-900"
-              >
-                {message}
-              </Dialog.Title>
-
-              {options?.children}
             </div>
-          </ModalTransition>
+            {/*body*/}
+            <div className="relative flex-auto">{options?.children}</div>
+            {/*footer*/}
+            <div className="flex items-center justify-end rounded-b  py-3 px-5">
+              <button
+                className="background-transparent mr-1 mb-1 px-4 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
+                type="button"
+                onClick={onCancel}
+              >
+                취소
+              </button>
+              <button
+                className="mr-1 mb-1 rounded bg-blue-500 px-4 py-2 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
+                type="button"
+                onClick={() => onConfirm}
+              >
+                확인
+              </button>
+            </div>
+          </div>
         </div>
-      </Dialog>
-    </Transition>
-  )
+      </div>
+      <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+    </>
+  ) : null
 }
