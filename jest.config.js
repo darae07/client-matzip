4d79ec5dce0e8d28ad45610ae6b0ce932ab3ebe1
darae07 <leedr0730@gmail.com>
2022-04-28 08:53:22 +0900

    testing-library 환경설정

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+const { defaults } = require('jest-config');
+module.exports = {
+    moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+    // Add more setup options before each test is run
+    moduleNameMapper: {
+        // Handle CSS imports (with CSS modules)
+        // https://jestjs.io/docs/webpack#mocking-css-modules
+        '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+        // Handle CSS imports (without CSS modules)
+        '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+        // Handle image imports
+        // https://jestjs.io/docs/webpack#handling-static-assets
+        '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`,
+        '^@/(.*)$': '<rootDir>/$1'
+    },
+    moduleDirectories: ["node_modules",],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+    testEnvironment: 'jsdom',
+    transform: {
+        // Use babel-jest to transpile tests with the next/babel preset
+        // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+        "^.+\\.svg$": "<rootDir>/svgTransform.js"
+    },
+    preset: "ts-jest",
+    transformIgnorePatterns: [
+        '/node_modules/',
+        '^.+\\.module\\.(css|sass|scss)$',
+    ],
+    testMatch: [
+        '<rootDir>/**/*.test.(js|jsx|ts|tsx)', '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+    ],
+    globals: {
+        'ts-jest': {
+            tsconfig: "tsconfig.json",
+            babelConfig: '.babelrc'
+        }
+    }
+};
