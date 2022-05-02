20b9c6ce3d0730c5e13aadca2e463cb8c0328d50
darae07 <leedr0730@gmail.com>
2022-05-02 22:18:37 +0900

    list item 페이지로 올림

--- pages/party/index.tsx
+++ pages/party/index.tsx
@@ -1,15 +1,19 @@
-import HomeLayout from 'components/layout/HomeLayout'
+import HomeLayout from '@/components/layout/HomeLayout'
 import { ReactElement } from 'react'
 import { NextPageWithLayout } from 'type/ui'
 import { WhiteRoundedCard } from 'components/card/styledCard'
 import { useAppSelector } from '@/utils/hooks'
 import Link from 'next/link'
-import PartyItemList from 'components/modules/party/PartyItemList'
-import MyPartyItemList from 'components/modules/party/MyPartyItemList'
 import CategoryFilter from 'components/modules/party/CategoryFilter'
 import { useRouter } from 'next/router'
 import _ from 'lodash'
 import PartyDetailModal from 'components/modal/party/PartyDetailModal'
+import { useQuery } from 'react-query'
+import { PaginatedResult } from 'type/api'
+import { Party } from 'type/party'
+import { listParty } from '@/api/party'
+import { ListItem } from '@/components/listItem'
+import { UserAvatarTooltip } from '@/components/user/UserAvatar'
 
 const PartyPage: NextPageWithLayout = () => {
   const user = useAppSelector((state) => state.user)
@@ -20,25 +24,65 @@ const PartyPage: NextPageWithLayout = () => {
 
   const showDetailModal = !_.isNil(detailId)
 
+  const { data, error, isLoading } = useQuery<PaginatedResult<Party>>(
+    ['party'],
+    listParty,
+    { enabled: !!team_profile }
+  )
+
+  if (team_profile) {
+    return (
+      <div>
+        <CategoryFilter />
+        <div className="mb-4"></div>
+        <ul className="grid gap-4 md:grid-cols-3">
+          {data?.results?.map((party: Party) => (
+            <Link
+              href={`/party?id=${party.id}`}
+              as={`/party/${party.id}`}
+              scroll={false}
+              passHref
+              key={party.id}
+            >
+              <ListItem >
+                <div className="mb-1 flex items-center">
+                  <span className="mr-2 rounded border border-blue-500 p-1 text-xs text-blue-500">
+                    {party.keyword.category?.name}
+                  </span>
+                  <p className="text-lg font-bold">{party.name}</p>
+                </div>
+
+                <span className="text-blue-500">#{party.keyword.name}</span>
+
+                <div className="my-4 flex -space-x-1 border border-white border-y-gray-200 py-3">
+                  {party.membership.map((membership) => (
+                    <UserAvatarTooltip
+                      user={membership.team_member}
+                      key={membership.id}
+                    />
+                  ))}
+                </div>
+              </ListItem>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  else {
+    <WhiteRoundedCard className="mb-4 cursor-pointer">
+      <Link href="/team" passHref>
+        <div>
+          <span className="mr-2 text-3xl">👋</span> 계정에 회사를 연동하면
+          동료들이 오늘 먹고싶은 점심 메뉴를 볼 수 있어요
+        </div>
+      </Link>
+    </WhiteRoundedCard>
+  }
+
   return (
     <div>
-      {!team_profile && (
-        <WhiteRoundedCard className="mb-4 cursor-pointer">
-          <Link href="/team" passHref>
-            <div>
-              <span className="mr-2 text-3xl">👋</span> 계정에 회사를 연동하면
-              동료들이 오늘 먹고싶은 점심 메뉴를 볼 수 있어요
-            </div>
-          </Link>
-        </WhiteRoundedCard>
-      )}
-      {team_profile && (
-        <div>
-          <CategoryFilter />
-          <div className="mb-4"></div>
-          <PartyItemList />
-        </div>
-      )}{' '}
       {showDetailModal && <PartyDetailModal id={detailId} />}
     </div>
   )
