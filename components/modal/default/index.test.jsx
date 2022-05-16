9daa21787959954e36eb2b3d22ff28e114a4298e
darae07 <leedr0730@gmail.com>
2022-05-16 13:10:40 +0900

    [test]모달 테스트 수정: 모달 isOpen props

--- components/modal/default/index.test.jsx
+++ components/modal/default/index.test.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
-import { Modal } from './Modal'
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { Modal } from "./Modal";
 
-describe('일반 모달 컴포넌트', () => {
-    it('일반 모달 컴포넌트는 children을 렌더링한다.', () => {
-        const { getByText } = render(<Modal isOpen={true} >
-            <div>test</div>
-        </Modal>)
-        const modal = screen.queryByRole('modal')
-        expect(modal).toBeInTheDocument()
-        expect(getByText('test')).toBeTruthy();
-    })
-    it('일반 모달 컴포넌트는 닫기 버튼 클릭시 unmount되며 handleClose이 실행된다.', () => {
-        const handleClose = jest.fn()
+describe("일반 모달 컴포넌트", () => {
+  it("일반 모달 컴포넌트는 children을 렌더링하고, 닫기 버튼 클릭시 unmount되며 handleClose이 실행된다.", () => {
+    const handleClose = jest.fn();
 
-        const { getByRole } = render(<Modal isOpen={true} handleClose={handleClose} />)
+    const { getByText, getByRole, rerender } = render(
+      <Modal isOpen={true} handleClose={handleClose}>
+        <div>test</div>
+      </Modal>
+    );
+    const modal = screen.queryByRole("modal");
+    expect(modal).toBeInTheDocument();
+    expect(getByText("test")).toBeTruthy();
 
-        fireEvent.click(getByRole('button', { name: 'close' }))
-        expect(handleClose).toHaveBeenCalledTimes(1)
-        expect(modal).not.toBeInTheDocument()
-    })
-})
+    fireEvent.click(getByRole("button", { name: "close" }));
+
+    expect(handleClose).toHaveBeenCalledTimes(1);
+    rerender(<Modal isOpen={false} handleClose={handleClose} />); // update props of a rendered component
+    expect(screen.queryByRole("modal")).not.toBeInTheDocument();
+  });
+});
