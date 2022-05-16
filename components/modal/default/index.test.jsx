d386c72b98f20c72fa2fb0bb99792bcc77c3ea4f
darae07 <leedr0730@gmail.com>
2022-05-16 11:28:58 +0900

    [test]모달 테스트 수정

--- components/modal/default/index.test.jsx
+++ components/modal/default/index.test.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import { Modal } from './Modal'
 
 describe('일반 모달 컴포넌트', () => {
-    it('일반 모달 컴포넌트는 children을 렌더링하며 닫기 버튼 클릭시 handleClose이 실행된다.', () => {
-        const handleClose = jest.fn()
-
-        const { getByRole, getByText } = render(<Modal isOpen={true} handleClose={handleClose}>
+    it('일반 모달 컴포넌트는 children을 렌더링한다.', () => {
+        const { getByText } = render(<Modal isOpen={true} >
             <div>test</div>
         </Modal>)
+        const modal = screen.queryByRole('modal')
+        expect(modal).toBeInTheDocument()
         expect(getByText('test')).toBeTruthy();
+    })
+    it('일반 모달 컴포넌트는 닫기 버튼 클릭시 unmount되며 handleClose이 실행된다.', () => {
+        const handleClose = jest.fn()
+
+        const { getByRole } = render(<Modal isOpen={true} handleClose={handleClose} />)
 
         fireEvent.click(getByRole('button', { name: 'close' }))
         expect(handleClose).toHaveBeenCalledTimes(1)
+        expect(modal).not.toBeInTheDocument()
     })
 })
