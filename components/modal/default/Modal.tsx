27d808a6960aefde21109713383f977ab73b8a86
darae07 <leedr0730@gmail.com>
2022-05-16 13:13:24 +0900

    일반 모달 컴포넌트 test 만족

--- components/modal/default/Modal.tsx
+++ components/modal/default/Modal.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React from 'react'
+import { Portal, defaultModalRootId } from '../portal'
 
-export const Modal = () => {
-    return (<div role='modal'></div>)
+export interface ModalProps {
+  isOpen: boolean
+  children?: Element
+  handleClose?: Function
+}
+
+export const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  children,
+  handleClose,
+}) => {
+  const closeButtonClicked = () => {
+    if (handleClose) handleClose()
+  }
+  if (!isOpen) return null
+
+  return (
+    <Portal rootId={defaultModalRootId}>
+      <div role="modal">
+        {children}
+        <button
+          className="float-right"
+          name="close"
+          onClick={closeButtonClicked}
+        >
+          close
+        </button>
+      </div>
+    </Portal>
+  )
 }
---
+++ components/modal/portal.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, useLayoutEffect, useState } from "react"
+import { createPortal } from "react-dom"
+
+type Props = {
+  children: Element | ReactNode
+  rootId: string
+}
+export const defaultModalRootId = 'modal-root'
+export const confirmModalRootId = 'confirm-modal-root'
+
+export const Portal: React.FC<Props> = ({ children, rootId }: Props) => {
+  const [rootElement, setRootElement] = useState<HTMLElement | null>(null)
+
+  useLayoutEffect(() => {
+    let root = document.getElementById(rootId)
+    let systemCreated = false
+    if (!root) {
+      systemCreated = true
+      const rootMount = document.createElement('div')
+      rootMount.setAttribute('id', rootId)
+      document.body.appendChild(rootMount)
+      root = rootMount
+    }
+    setRootElement(root)
+
+    return () => {
+      if (systemCreated && root?.parentNode) {
+        root.parentNode.removeChild(root)
+      }
+    }
+  }, [rootId])
+
+  if (rootElement === null) return null
+  return createPortal(children, rootElement)
+}
