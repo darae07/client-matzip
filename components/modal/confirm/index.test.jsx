e2de41d594fe7aa940a0750d44542906b1261d2f
darae07 <leedr0730@gmail.com>
2022-05-26 13:33:08 +0900

    [test]에러 수정 및 react 18 업그레이드

--- components/modal/confirm/index.test.jsx
+++ components/modal/confirm/index.test.jsx
@@ -1,10 +1,12 @@
 import { fireEvent, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 import { confirm } from './index'
 
 describe('컨펌 모달 컴포넌트', () => {
   it('컨펌 모달 컴포넌트는 message를 렌더링하고, confirm 함수 호출시 마운트되며, 확인 버튼을 누르면 true를 반환한 후 언마운트 된다.', async () => {
-    let userResponse = confirm('ok?')
-
+    act(() => {
+      let userResponse = confirm('ok?')
+    })
     const okButton = await screen.findByRole('button', { name: '확인' })
     expect(okButton).toBeInTheDocument()
     fireEvent.click(okButton)
