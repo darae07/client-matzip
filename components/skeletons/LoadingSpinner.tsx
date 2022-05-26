9f3d0186b79e91a92dd69cf1d0846294835f916f
darae07 <leedr0730@gmail.com>
2022-05-26 17:35:52 +0900

    로딩 컴포넌트 수정

--- components/skeletons/LoadingSpinner.tsx
+++ components/skeletons/LoadingSpinner.tsx
@@ -1,22 +1,23 @@
-import React, { FC } from 'react'
+import React, { FC, HTMLAttributes } from 'react'
 import LoadingSpinnerIcon from '@/public/icon/loading_spinner.svg'
 
-interface IconProps {
-  width: number | string | null
-  height: number | string | null
-  className: string | null
+interface IconProps extends HTMLAttributes<HTMLDivElement> {
+  width?: number | string
+  height?: number | string
 }
 
 export const LoadingSpinner: FC<IconProps> = ({
   width = 24,
   height = 16,
   className,
+  role,
 }) => {
   return (
     <LoadingSpinnerIcon
       width={width}
       height={height}
-      className={`animate-spin ${className}`}
+      className="animate-spin"
+      role={role}
     />
   )
 }
