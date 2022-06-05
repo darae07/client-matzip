6d19771083bc6cf0ac003276c0d50806a8187419
darae07 <leedr0730@gmail.com>
2022-06-05 16:49:23 +0900

    리뷰 모달 파티 종료 연결

--- components/modules/keyword/ReviewModal.tsx
+++ components/modules/keyword/ReviewModal.tsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as Yup from 'yup'
-import { Modal, GoodButton, Form, FormInput, FormFileInput } from '@/components'
+import {
+  Modal,
+  GoodButton,
+  Form,
+  FormTextarea,
+  FormFileInput,
+} from '@/components'
+import { ApiResponseData, Party, ReviewCreateValue } from '@/type'
+import classNames from 'classnames'
+import { useMutationHandleError } from '@/utils/hooks'
+import { closeParty, closePartyWithReview } from '@/api'
+import { openToast } from '@/components/toast'
+import { useQueryClient } from 'react-query'
 
 interface ReviewModalProps {
   isOpen: boolean
   setOpen: Function
-  domain: string
-}
-type ReviewCreateValue = {
-  content: string
-  image: []
+  domain: 'party' | 'lunch'
 }
 
 const reviewValues: ReviewCreateValue = {
@@ -21,17 +30,63 @@ const reviewValues: ReviewCreateValue = {
 export const ReviewModal = ({ isOpen, setOpen, domain }: ReviewModalProps) => {
   const closeModal = () => setOpen(false)
   const [isGood, setIsGood] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const { query } = useRouter()
+  const { id } = query
 
   const createReviewSchema = Yup.object().shape({})
   const handleCreateReview = (values: ReviewCreateValue) => {
-    console.log(values)
+    if (isSubmitting) return
+    setIsSubmitting(true)
+    if (isGood) {
+      const formData = new FormData()
+      formData.append('content', values.content)
+      values.image.forEach((img) => formData.append('image', img))
+      if (domain === 'party')
+        closePartyWithReviewMutation.mutate({ id, data: formData })
+    } else {
+      if (domain === 'party') closePartyMutation.mutate(id)
+    }
   }
+
+  const queryClient = useQueryClient()
+  const closePartyMutation = useMutationHandleError(
+    closeParty,
+    {
+      onSuccess: (data: ApiResponseData<Party>) => {
+        const { message, result } = data
+        openToast(message || '식사 완료 기록을 생성했습니다.')
+        queryClient.invalidateQueries('partyItem')
+        closeModal()
+      },
+      onSettled: (data: ApiResponseData<Party>) => {
+        setIsSubmitting(false)
+      },
+    },
+    '식사 완료 기록을 생성할 수 없습니다.',
+  )
+  const closePartyWithReviewMutation = useMutationHandleError(
+    closePartyWithReview,
+    {
+      onSuccess: (data: ApiResponseData<Party>) => {
+        const { message, result } = data
+        openToast(message || '식사 리뷰를 등록했습니다.')
+        queryClient.invalidateQueries('partyItem')
+        closeModal()
+      },
+      onSettled: (data: ApiResponseData<Party>) => {
+        setIsSubmitting(false)
+      },
+    },
+    '식사 리뷰를 등록할 수 없습니다.',
+  )
 
   return (
     <Modal handleClose={closeModal} isOpen={isOpen} title="식사는 어떠셨나요?">
       <div className="mt-4 flex flex-col items-center">
         <GoodButton isGood={isGood} setIsGood={setIsGood} />
-        <div className="mt-6 w-3/4">
+        <div className={classNames('mt-6 w-3/4', { hidden: !isGood })}>
           <p className="mb-2">
             맛있으셨나요? 동료들에게 추천 메시지를 남겨주세요
           </p>
@@ -42,7 +97,7 @@ export const ReviewModal = ({ isOpen, setOpen, domain }: ReviewModalProps) => {
               defaultValues: reviewValues,
             }}
           >
-            <FormInput<ReviewCreateValue>
+            <FormTextarea<ReviewCreateValue>
               name="content"
               placeholder="코멘트를 남겨주세요"
             />
@@ -54,14 +109,23 @@ export const ReviewModal = ({ isOpen, setOpen, domain }: ReviewModalProps) => {
               mode="append"
               className="mt-3"
             />
-            <input type="submit" id="submitForm" className="hidden" />
+            <input
+              type="submit"
+              id="submitForm"
+              className="hidden"
+              disabled={isSubmitting}
+            />
           </Form>
         </div>
 
-        <div className="mt-4 w-full">
+        <div className="mt-8 w-full">
           <label
+            aria-disabled={isSubmitting}
             htmlFor="submitForm"
-            className="block w-full rounded bg-pink-500 p-2 px-3 text-center text-white"
+            className={classNames(
+              'block w-full rounded bg-pink-500 p-2 px-3 text-center text-white',
+              { 'bg-gray-500': isSubmitting },
+            )}
           >
             먹었어요
           </label>
