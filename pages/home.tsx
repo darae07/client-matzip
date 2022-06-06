722b85ba6a1f1319a78da834dfe636becb960110
darae07 <leedr0730@gmail.com>
2022-06-06 23:20:04 +0900

    [code-style]

--- pages/home.tsx
+++ pages/home.tsx
@@ -11,7 +11,7 @@ import {
 const Home: NextPageWithLayout = () => {
   return (
     <div>
-      <WhiteRoundedCard className="mb-4" role='today'> 
+      <WhiteRoundedCard className="mb-4" role="today">
         오늘 뭐 먹지? 직장내 쉽고 빠른 점심메뉴 의사결정을 경험해 보세요
       </WhiteRoundedCard>
       <WhiteRoundedCard className="mb-4">
