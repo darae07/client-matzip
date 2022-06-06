e2dda7b4f3662b148483879c094c1c107481facc
darae07 <leedr0730@gmail.com>
2022-06-06 18:14:31 +0900

    [style] 리스트 아이템 스타일 추가

--- components/card/styledCard.ts
+++ components/card/styledCard.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import tw from 'twin.macro'
 
-export const WhiteRoundedCard = styled.div`
-  ${tw`rounded-lg bg-white py-6 px-7 text-gray-800 shadow`}
+export const WhiteRoundedCard = styled.div<{ flatTop?: boolean }>`
+  ${tw` bg-white py-6 px-7 text-gray-800 shadow`}
+  ${(props) => (props.flatTop ? tw`rounded-b-lg` : tw`rounded-lg`)}
 `
