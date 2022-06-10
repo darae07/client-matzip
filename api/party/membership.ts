68a6a334efa5567367a0e836083d88c66ce26fd5
darae07 <leedr0730@gmail.com>
2022-06-11 00:07:25 +0900

    [api]파티 초대 거절, 수락, 취소

--- api/party/membership.ts
+++ api/party/membership.ts
@@ -18,3 +18,33 @@ export const inviteParty = async <ResultT>(values: InvitePartyValue) => {
     await authorizedInstance.post('group/party_membership/invite/', values)
   return response
 }
+
+export const myInviteList = async <ResultT>(page: number = 1) => {
+  const { data: response }: ApiResponse<ResultT> = await authorizedInstance.get(
+    'group/party_membership/my_invite_list/',
+    {
+      params: {
+        page,
+      },
+    },
+  )
+  return response.result
+}
+
+export const acceptInviteParty = async <ResultT>(id: number) => {
+  const { data: response }: ApiResponse<ResultT> =
+    await authorizedInstance.post(`group/party_membership/${id}/accept/`)
+  return response
+}
+
+export const refuseInviteParty = async <ResultT>(id: number) => {
+  const { data: response }: ApiResponse<ResultT> =
+    await authorizedInstance.post(`group/party_membership/${id}/refuse/`)
+  return response
+}
+
+export const cancelInviteParty = async <ResultT>(id: number) => {
+  const { data: response }: ApiResponse<ResultT> =
+    await authorizedInstance.post(`group/party_membership/${id}/cancel/`)
+  return response
+}
