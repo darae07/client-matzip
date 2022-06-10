82a14ebd7f5a06257e054a7e6aea859b0d462423
darae07 <leedr0730@gmail.com>
2022-06-10 13:16:41 +0900

    폼 자동제출 추가

--- components/controls/forms/Form.tsx
+++ components/controls/forms/Form.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   useForm,
   UseFormProps,
   SubmitHandler,
   FormProvider,
+  UnpackNestedValue,
 } from 'react-hook-form'
 
 type FormProps<TFomValues> = {
   options?: UseFormProps<TFomValues>
   onSubmit: SubmitHandler<TFomValues>
   children: any
+  autoSubmit?: boolean
 }
 export const Form = <TFomValues extends Record<string, unknown>>({
   options,
   onSubmit,
   children,
+  autoSubmit = false,
 }: FormProps<TFomValues>) => {
   const methods = useForm<TFomValues>(options)
+  const handleAutoSubmit = (data: UnpackNestedValue<TFomValues>) =>
+    onSubmit(data)
+  const values = methods.watch()
+
+  useEffect(() => {
+    if (autoSubmit) {
+      handleAutoSubmit(values)
+    }
+  }, [values])
+
   return (
     <FormProvider<TFomValues> {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>
