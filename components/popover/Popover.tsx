bd43fd2e135a33e53d16571fd8d0d8e2bb617bde
darae07 <leedr0730@gmail.com>
2022-06-14 22:12:42 +0900

    팝오버 컴포넌트 조합

--- components/popover/Popover.tsx
+++ components/popover/Popover.tsx
@@ -1,20 +1,27 @@
+import { createPopper } from '@popperjs/core'
 import React, {
   createContext,
   useContext,
   useState,
   HTMLAttributes,
+  createRef,
 } from 'react'
+import classNames from 'classnames'
 
 interface PopoverContextValue {
   isOpen: boolean
   handleOpen: Function
   handleClose: Function
+  btnDropdownRef: any
+  popoverDropdownRef: any
 }
 
 const PopoverContextInitialValue = {
   isOpen: false,
   handleOpen: () => {},
   handleClose: () => {},
+  btnDropdownRef: null,
+  popoverDropdownRef: null,
 }
 const PopoverContext = createContext<PopoverContextValue>(
   PopoverContextInitialValue,
@@ -34,25 +41,75 @@ interface PopoverProps extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactChild
   tooltipText?: string
 }
-const Popover = ({ children }: PopoverProps) => {
+export const Popover = ({ children }: PopoverProps) => {
   const [isOpen, setOpen] = useState(false)
-  const handleOpen = () => setOpen(true)
+
+  const btnDropdownRef = createRef<HTMLDivElement>()
+  const popoverDropdownRef = createRef<HTMLDivElement>()
+  const handleOpen = () => {
+    if (!btnDropdownRef.current || !popoverDropdownRef.current) return
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: 'bottom-end',
+    })
+    setOpen(true)
+  }
   const handleClose = () => setOpen(false)
 
-  const contextValue = { isOpen, handleClose, handleOpen }
+  const contextValue = {
+    isOpen,
+    handleClose,
+    handleOpen,
+    btnDropdownRef,
+    popoverDropdownRef,
+  }
   return (
-    <PopoverContext.Provider value={contextValue}>
-      {children}
-    </PopoverContext.Provider>
+    <div className=" relative">
+      <PopoverContext.Provider value={contextValue}>
+        {children}
+      </PopoverContext.Provider>
+    </div>
   )
 }
 
-const Button = () => {
-  const { isOpen, handleOpen, handleClose } = usePopoverContext()
+interface PopoverButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactChild
+}
+const Button = ({ children }: PopoverButtonProps) => {
+  const { isOpen, handleOpen, handleClose, btnDropdownRef } =
+    usePopoverContext()
+
+  const handleButtonClick = () => {
+    if (isOpen) {
+      handleClose()
+    } else {
+      handleOpen()
+    }
+  }
+  return (
+    <button type="button" onClick={handleButtonClick} ref={btnDropdownRef}>
+      {children}
+    </button>
+  )
 }
 
-const Panel = () => {
-  const { isOpen, handleOpen, handleClose } = usePopoverContext()
+interface PopoverPanelProps extends HTMLAttributes<HTMLDivElement> {
+  children: React.ReactChild
+}
+const Panel = ({ children, className }: PopoverPanelProps) => {
+  const { isOpen, handleOpen, handleClose, popoverDropdownRef } =
+    usePopoverContext()
+
+  return (
+    <div
+      ref={popoverDropdownRef}
+      className={classNames(
+        'absolute block w-screen min-w-[12rem] bg-white',
+        className,
+      )}
+    >
+      {isOpen && children}
+    </div>
+  )
 }
 
 Popover.Button = Button
