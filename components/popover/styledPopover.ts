046ec20c33f1e0e2c8eb3ea0aef2257126c913e0
darae07 <leedr0730@gmail.com>
2022-06-14 22:13:03 +0900

    [style] 팝오버

--- components/popover/styledPopover.ts
+++ components/popover/styledPopover.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 
 export const PopoverContainer = styled.div`
-  ${tw`overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5`}
+  ${tw`overflow-hidden h-screen sm:h-full rounded-lg shadow-lg ring-1 ring-black ring-opacity-5`}
 `
 export const PopoverItem = styled.a`
-  ${tw` flex items-center text-gray-600 rounded-lg p-3 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none `}
+  ${tw` flex items-center text-gray-600 rounded-lg p-3 pr-7 sm:pr-3 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none `}
 `
