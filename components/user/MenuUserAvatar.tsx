08c05a615718da1f0ace6c7f5fdf2a812a63f372
darae07 <leedr0730@gmail.com>
2022-06-14 23:45:06 +0900

    [style] 팝오버 멀티 차일드

--- components/user/MenuUserAvatar.tsx
+++ components/user/MenuUserAvatar.tsx
@@ -91,120 +91,112 @@ const MenuUserAvatar: FC = () => {
     <div className="inline-block">
       {user ? (
         <Popover>
-          <>
-            <Popover.Button>
-              {user.team_profile && user.team_profile.image ? (
-                <Image
-                  src={user.team_profile.image}
-                  width={24}
-                  height={24}
-                  className="h-6 w-6 rounded-full"
-                  alt="user-image"
-                />
-              ) : (
-                <UserCircleIcon className="h-6 w-6" />
-              )}
-            </Popover.Button>
+          <Popover.Button>
+            {user.team_profile && user.team_profile.image ? (
+              <Image
+                src={user.team_profile.image}
+                width={24}
+                height={24}
+                className="h-6 w-6 rounded-full"
+                alt="user-image"
+              />
+            ) : (
+              <UserCircleIcon className="h-6 w-6" />
+            )}
+          </Popover.Button>
 
-            <Popover.Panel className="absolute right-0 z-20 mt-1 sm:w-[14rem] sm:px-0 ">
-              <PopoverContainer>
-                {user.team_profile ? (
-                  <PopoverItem href="/">
-                    <UserCircleIcon className="h-4 w-4" />
-                    <div className="ml-4">
-                      <p className="font-semibold">
-                        {user.team_profile.member_name}
-                      </p>
-                      <p className="text-sm font-medium ">내 프로필 보기</p>
-                    </div>
-                  </PopoverItem>
-                ) : (
-                  <PopoverItem href="/team">
-                    <UserCircleIcon className="h-4 w-4" />
-                    <div className="ml-4">
-                      <p className="text-sm font-medium ">
-                        계정에 회사 연동하기
-                      </p>
-                    </div>
-                  </PopoverItem>
-                )}
-                {data && (
-                  <div>
-                    {!!data.pages[0].count && (
-                      <p className="ml-1 border-t border-t-gray-300 px-10 pt-4 text-sm font-bold text-gray-600">
-                        나의 파티 초대 요청
-                      </p>
-                    )}
-                    {data.pages.map((group, i) => (
-                      <Fragment key={i}>
-                        {group.results.map((membership: PartyMembership) => (
-                          <PopoverItem key={membership.id}>
-                            <div className="flex w-full items-center justify-between">
-                              <div className="flex items-center">
-                                <UserAvatar user={membership.team_member} />
-                                <span className="ml-2 text-sm font-medium">
-                                  {membership.team_member.member_name}
-                                </span>
-                              </div>
-                              <div className="float-right text-xs">
-                                {membership.team_member.id ===
-                                  user?.team_profile?.id && (
-                                  <>
-                                    <button
-                                      onClick={() =>
-                                        handleAccept(membership.id)
-                                      }
-                                      type="button"
-                                      className="mr-1 rounded bg-blue-100 p-1 text-blue-800"
-                                    >
-                                      수락
-                                    </button>
-                                    <button
-                                      onClick={() =>
-                                        handleRefuse(membership.id)
-                                      }
-                                      type="button"
-                                      className="rounded bg-red-100 p-1 text-red-800"
-                                    >
-                                      거절
-                                    </button>
-                                  </>
-                                )}
-                                {membership.invite_member?.id ===
-                                  user?.team_profile?.id && (
-                                  <button
-                                    onClick={() => handleCancel(membership.id)}
-                                    type="button"
-                                    className="rounded bg-gray-100 p-1 text-gray-600"
-                                  >
-                                    요청 취소
-                                  </button>
-                                )}
-                              </div>
-                            </div>
-                          </PopoverItem>
-                        ))}
-                      </Fragment>
-                    ))}
-                  </div>
-                )}
-
-                <div className="w-full border border-b-gray-50"></div>
+          <Popover.Panel className="absolute right-0 z-20 mt-1 sm:w-[14rem] sm:px-0 ">
+            <PopoverContainer>
+              {user.team_profile ? (
                 <PopoverItem href="/">
-                  <KeyIcon className="h-4 w-4" />
+                  <UserCircleIcon className="h-4 w-4" />
                   <div className="ml-4">
-                    <p className="text-sm font-medium ">계정 설정</p>
+                    <p className="font-semibold">
+                      {user.team_profile.member_name}
+                    </p>
+                    <p className="text-sm font-medium ">내 프로필 보기</p>
                   </div>
                 </PopoverItem>
-                <PopoverItem onClick={handleLogout}>
-                  <LogoutIcon className="h-4 w-4 text-red-500" />
-                  <div className="ml-4 hover:cursor-pointer">
-                    <p className="text-sm font-medium text-red-500">로그아웃</p>
+              ) : (
+                <PopoverItem href="/team">
+                  <UserCircleIcon className="h-4 w-4" />
+                  <div className="ml-4">
+                    <p className="text-sm font-medium ">계정에 회사 연동하기</p>
                   </div>
                 </PopoverItem>
-              </PopoverContainer>
-            </Popover.Panel>
-          </>
+              )}
+              {data && (
+                <div>
+                  {!!data.pages[0].count && (
+                    <p className="ml-1 border-t border-t-gray-300 px-10 pt-4 text-sm font-bold text-gray-600">
+                      나의 파티 초대 요청
+                    </p>
+                  )}
+                  {data.pages.map((group, i) => (
+                    <Fragment key={i}>
+                      {group.results.map((membership: PartyMembership) => (
+                        <PopoverItem key={membership.id}>
+                          <div className="flex w-full items-center justify-between">
+                            <div className="flex items-center">
+                              <UserAvatar user={membership.team_member} />
+                              <span className="ml-2 text-sm font-medium">
+                                {membership.team_member.member_name}
+                              </span>
+                            </div>
+                            <div className="float-right text-xs">
+                              {membership.team_member.id ===
+                                user?.team_profile?.id && (
+                                <>
+                                  <button
+                                    onClick={() => handleAccept(membership.id)}
+                                    type="button"
+                                    className="mr-1 rounded bg-blue-100 p-1 text-blue-800"
+                                  >
+                                    수락
+                                  </button>
+                                  <button
+                                    onClick={() => handleRefuse(membership.id)}
+                                    type="button"
+                                    className="rounded bg-red-100 p-1 text-red-800"
+                                  >
+                                    거절
+                                  </button>
+                                </>
+                              )}
+                              {membership.invite_member?.id ===
+                                user?.team_profile?.id && (
+                                <button
+                                  onClick={() => handleCancel(membership.id)}
+                                  type="button"
+                                  className="rounded bg-gray-100 p-1 text-gray-600"
+                                >
+                                  요청 취소
+                                </button>
+                              )}
+                            </div>
+                          </div>
+                        </PopoverItem>
+                      ))}
+                    </Fragment>
+                  ))}
+                </div>
+              )}
+
+              <div className="w-full border border-b-gray-50"></div>
+              <PopoverItem href="/">
+                <KeyIcon className="h-4 w-4" />
+                <div className="ml-4">
+                  <p className="text-sm font-medium ">계정 설정</p>
+                </div>
+              </PopoverItem>
+              <PopoverItem onClick={handleLogout}>
+                <LogoutIcon className="h-4 w-4 text-red-500" />
+                <div className="ml-4 hover:cursor-pointer">
+                  <p className="text-sm font-medium text-red-500">로그아웃</p>
+                </div>
+              </PopoverItem>
+            </PopoverContainer>
+          </Popover.Panel>
         </Popover>
       ) : (
         <Link href="/login">로그인</Link>
