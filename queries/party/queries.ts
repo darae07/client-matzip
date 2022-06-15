df3efffa90956416a588cbcade52a6a0c8aa5afb
darae07 <leedr0730@gmail.com>
2022-06-15 16:41:36 +0900

    [query] 파티 쿼리

--- queries/party/queries.ts
+++ queries/party/queries.ts
@@ -1,6 +1,21 @@
-import { retrieveParty } from '@/api'
-import { Party } from '@/type'
-import { useQuery } from 'react-query'
+import { listParty, retrieveParty } from '@/api'
+import { PaginatedResult, Party, PartyList, TeamMember } from '@/type'
+import { useInfiniteQuery, useQuery } from 'react-query'
+
+export const usePartyQuery = (
+  category?: number,
+  team_profile?: TeamMember | null,
+) =>
+  useInfiniteQuery<PaginatedResult<PartyList>>(
+    ['party', category],
+    ({ pageParam = 1 }) => listParty(pageParam, category),
+    {
+      enabled: !!team_profile,
+      keepPreviousData: true,
+      getNextPageParam: (lastPage, pages) => lastPage.next,
+      cacheTime: 1000 * 60 * 60,
+    },
+  )
 
 export const usePartyItemQuery = (id?: string | string[]) =>
   useQuery(['partyItem', id], () => retrieveParty<Party>(id), { enabled: !!id })
