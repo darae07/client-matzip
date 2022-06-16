7829afc114e9d1dee3b363e4c854407de9da9042
darae07 <leedr0730@gmail.com>
2022-06-16 15:04:10 +0900

    에러 페이지 작성

--- components/error/ErrorFallback.tsx
+++ components/error/ErrorFallback.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import { FallbackProps } from 'react-error-boundary'
+import { RefreshIcon } from '@heroicons/react/outline'
 
 type RenderFallbackProps<ErrorType extends Error = Error> = {
   error: ErrorType
@@ -12,10 +13,19 @@ type RenderFallbackType = <ErrorType extends Error>(
 
 export const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
+    <div
+      role="alert"
+      className="container mx-auto flex h-screen w-fit items-center"
+    >
+      <div className="text-center">
+        <h1 className="text-5xl font-black text-blue-400">
+          문제가 발생했습니다.
+        </h1>
+        <pre className="mt-5 text-2xl">{error.message}</pre>
+        <button onClick={resetErrorBoundary}>
+          <RefreshIcon className="mt-5 h-10 w-10" />
+        </button>
+      </div>
     </div>
   )
 }
---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+export default function PageNotFound() {
+  return (
+    <div className="container mx-auto flex h-screen w-fit items-center">
+      <div className="text-center">
+        <h2 className="text-7xl font-black text-blue-400">404</h2>
+        <h1 className="mt-5 text-2xl">페이지를 찾을 수 없습니다.</h1>
+      </div>
+    </div>
+  )
+}
