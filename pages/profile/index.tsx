64373a02744816bcf8a3ce463e29dc52c4e84a04
darae07 <leedr0730@gmail.com>
2022-06-20 19:17:45 +0900

    내 리뷰 목록

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,18 +1,47 @@
-import { ReactElement, useEffect } from 'react'
-import { HomeLayout, UserAvatar, WhiteRoundedCard } from '@/components'
-import { NextPageWithLayout } from '@/type'
+import { Fragment, ReactElement, useEffect, useState } from 'react'
+import {
+  HomeLayout,
+  InfiniteScroll,
+  ListItem,
+  LoadingSpinner,
+  UserAvatar,
+  WhiteRoundedCard,
+  YLineCard,
+} from '@/components'
+import { NextPageWithLayout, Review, ReviewImage } from '@/type'
 import { useAppSelector } from '@/utils/hooks'
 import { useRouter } from 'next/router'
 import { PencilIcon } from '@heroicons/react/outline'
+import { useMyReviewQuery } from '@/queries'
+import { printDateTimeForToday } from '@/utils'
+import Image from 'next/image'
+import { KeywordScoreIcon, ReviewDetailModal } from '@/components/modules'
 
 const ProfilePage: NextPageWithLayout = () => {
-  const { isLoading, user } = useAppSelector((state) => state.user)
+  const { user } = useAppSelector((state) => state.user)
   const router = useRouter()
   useEffect(() => {
     if (!user) {
       router.back()
     }
   }, [user])
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    hasPreviousPage,
+    isLoading,
+    isFetching,
+    isFetchingNextPage,
+  } = useMyReviewQuery()
+
+  const [isOpenReviewDetailModal, setOpenReviewDetailModal] = useState(false)
+  const [reviewImageId, setReviewImageId] = useState<number>()
+  const handleSelectReviewImage = (id: number) => {
+    setReviewImageId(id)
+    setOpenReviewDetailModal(true)
+  }
 
   return (
     <div>
@@ -39,6 +68,84 @@ const ProfilePage: NextPageWithLayout = () => {
             </div>
           </div>
         </WhiteRoundedCard>
+      )}
+
+      {isLoading ? (
+        <div className="mt-10 mb-5 flex w-full justify-center">
+          <LoadingSpinner width={30} height={30} />
+        </div>
+      ) : (
+        data?.pages && (
+          <WhiteRoundedCard className="mt-4">
+            <p className="mb-5 text-xl">
+              내가 쓴 리뷰
+              <span className="ml-1 text-gray-500">
+                ({data.pages[0]?.count})
+              </span>
+            </p>
+            <ul>
+              {data.pages.map((group, i) => (
+                <Fragment key={i}>
+                  {group.results.map((item: Review) => (
+                    <ListItem
+                      key={item.id}
+                      isPreviousData={isFetching && !isFetchingNextPage}
+                    >
+                      <YLineCard className="relative">
+                        <div className="flex">
+                          <UserAvatar user={item.team_member} />
+                          <span className="ml-1">
+                            {item.team_member.member_name}
+                          </span>
+                        </div>
+                        <KeywordScoreIcon
+                          score={item.score}
+                          className="absolute right-0"
+                        />
+                        <p className="mt-1 text-sm text-gray-500">
+                          {printDateTimeForToday(item.created_at)}
+                        </p>
+                        <p className="my-4">{item.content}</p>
+
+                        <div className="flex">
+                          {item.images?.map((image: ReviewImage) => (
+                            <div
+                              className="mr-2"
+                              key={image.id}
+                              onClick={() => {
+                                handleSelectReviewImage(image.id)
+                              }}
+                            >
+                              <Image
+                                src={image.image}
+                                alt={image.image}
+                                width={120}
+                                height={120}
+                                className="rounded-lg"
+                              />
+                            </div>
+                          ))}
+                        </div>
+                      </YLineCard>
+                    </ListItem>
+                  ))}
+                </Fragment>
+              ))}
+            </ul>
+            <InfiniteScroll
+              fetchNextPage={fetchNextPage}
+              isFetchingNextPage={isFetchingNextPage}
+              hasNextPage={hasNextPage}
+            />
+            {reviewImageId && (
+              <ReviewDetailModal
+                reviewImageId={reviewImageId}
+                isOpen={isOpenReviewDetailModal}
+                setIsOpen={setOpenReviewDetailModal}
+              />
+            )}
+          </WhiteRoundedCard>
+        )
       )}
     </div>
   )
