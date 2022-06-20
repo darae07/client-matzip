ff26aeb3621bfc9c1324d53e89efe512dc95629d
darae07 <leedr0730@gmail.com>
2022-06-20 16:13:33 +0900

    [style] 버튼 화이트 컬러 추가

--- components/buttons/style.tsx
+++ components/buttons/style.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  color?: 'red' | 'blue' | 'pink'
+  color?: 'red' | 'blue' | 'pink' | 'white'
   size?: 'small' | 'medium'
   disabled?: boolean
 }
@@ -14,6 +14,9 @@ export const Button = styled.button.attrs((props) => ({
   ${(props) => props.color === 'red' && tw`text-red-500`}
   ${(props) => props.color === 'blue' && tw`bg-blue-500 active:bg-blue-600`}
   ${(props) => props.color === 'pink' && tw`bg-pink-500 active:bg-pink-600`}
+  ${(props) =>
+    props.color === 'white' &&
+    tw`bg-white border border-gray-400 active:bg-gray-200 text-gray-600`}
   ${(props) => (props.size === 'small' ? tw`p-2 px-3` : tw`px-4 py-2`)}
   ${(props) => props.disabled && tw`bg-gray-500`}
 `
