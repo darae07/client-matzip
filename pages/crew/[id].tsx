9b651dd178a26ca8c7a9b1a0471cee65fbb96a77
darae07 <leedr0730@gmail.com>
2022-06-23 16:23:49 +0900

    크루 멤버쉽 초대

--- pages/crew/[id].tsx
+++ pages/crew/[id].tsx
@@ -1,19 +1,28 @@
+import _ from 'lodash'
 import {
   Button,
   HomeLayout,
   LoadingSpinner,
+  Modal,
   openToast,
+  PlusButton,
+  SearchAndSelectUser,
   SmallBlueButton,
   UserAvatarTooltip,
   WhiteRoundedCard,
 } from '@/components'
-import { useCrewItemQuery } from '@/queries'
-import { NextPageWithLayout } from '@/type'
+import {
+  useCrewItemQuery,
+  useInviteCrewMutation,
+  useOutCrewMutation,
+} from '@/queries'
+import { CrewMembership, NextPageWithLayout } from '@/type'
 import { useRouter } from 'next/router'
-import { ReactElement, useEffect } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import { useQueryClient } from 'react-query'
 import Image from 'next/image'
 import { LightBulbIcon, PencilIcon, PlusIcon } from '@heroicons/react/outline'
+import { useAppSelector } from '@/utils/hooks'
 
 const CrewDetail: NextPageWithLayout = () => {
   const queryClient = useQueryClient()
@@ -32,6 +41,37 @@ const CrewDetail: NextPageWithLayout = () => {
       }
     }
   }, [error])
+
+  const user = useAppSelector((state) => state.user)
+  const team_profile = user.user?.team_profile
+  const teamId = team_profile?.team
+
+  const [myMembership, setMyMembership] = useState<CrewMembership>()
+  useEffect(() => {
+    if (data && data?.membership) {
+      const my_membership = _.find(data.membership, function (e) {
+        return e.team_member.id === team_profile?.id
+      })
+      setMyMembership(my_membership)
+    }
+  }, [data, team_profile])
+
+  const outMutation = useOutCrewMutation(setMyMembership, id, data)
+  const handleOutCrew = () => outMutation.mutate(myMembership?.id)
+
+  const [isSearchModalOpen, setSearchModalOpen] = useState(false)
+  const openSearchModal = () => setSearchModalOpen(true)
+  const closeSearchModal = () => setSearchModalOpen(false)
+
+  const inviteMutation = useInviteCrewMutation()
+  const userSelectAction = (receiverId: number) => {
+    const data = {
+      crew: id,
+      receiver: receiverId,
+    }
+    inviteMutation.mutate(data)
+    queryClient.invalidateQueries('member')
+  }
 
   const createLunch = () => {}
 
@@ -72,14 +112,34 @@ const CrewDetail: NextPageWithLayout = () => {
               <span>수정</span>
             </Button>
           </div>
-          <div className="my-4 flex -space-x-1 border border-white border-y-gray-200 py-3">
-            {data.membership.map((membership) => (
-              <UserAvatarTooltip
-                user={membership.team_member}
-                key={membership.id}
-                membership={membership}
-              />
-            ))}
+          <div className="my-4 flex justify-between border border-white border-y-gray-200 py-3">
+            <div className="flex -space-x-1">
+              {data.membership.map((membership) => (
+                <UserAvatarTooltip
+                  user={membership.team_member}
+                  key={membership.id}
+                  membership={membership}
+                />
+              ))}
+              {!!myMembership && <PlusButton onClick={openSearchModal} />}
+              <Modal handleClose={closeSearchModal} isOpen={isSearchModalOpen}>
+                <SearchAndSelectUser
+                  selectAction={userSelectAction}
+                  party={Number(id)}
+                  mutatonState={inviteMutation}
+                />
+              </Modal>
+            </div>
+            {!!myMembership && (
+              <Button
+                onClick={() => handleOutCrew()}
+                size="small"
+                color="blue"
+                disabled={outMutation.isLoading}
+              >
+                나가기
+              </Button>
+            )}
           </div>
         </WhiteRoundedCard>
 
