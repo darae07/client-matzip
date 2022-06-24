4e9edc947e6e7a5a4364fe916773408d8eb6268f
darae07 <leedr0730@gmail.com>
2022-06-24 16:19:57 +0900

    [query] 키워드 조회 수정

--- queries/keyword/queries.ts
+++ queries/keyword/queries.ts
@@ -1,18 +1,11 @@
 import { retrieveKeyword } from '@/api/keyword/read'
 import { ApiErrorResponse } from '@/type/api'
 import { Keyword } from '@/type/lunch'
-import { useAppSelector } from '@/utils/hooks'
 import { useQuery } from 'react-query'
 
-export const useKeywordItemQuery = (id?: string | string[]) => {
-  const { user } = useAppSelector((state) => state.user)
+export const useKeywordItemQuery = (id?: string | string[]) =>
   useQuery<void | Keyword, ApiErrorResponse<Keyword>>(
     ['keywordItem', id],
-    ({ pageParam = 1 }) => retrieveKeyword(pageParam),
-    {
-      enabled: !!user?.team_profile,
-      keepPreviousData: true,
-      cacheTime: 1000 * 60 * 60,
-    },
+    () => retrieveKeyword(id),
+    { enabled: !!id },
   )
-}
