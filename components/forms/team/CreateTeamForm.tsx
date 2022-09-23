4a2c2f161c07d52ede7f33b2fc0cc1f646679ffb
darae07 <leedr0730@gmail.com>
2022-09-23 09:36:08 +0900

    버튼 disable 추가

--- components/forms/team/CreateTeamForm.tsx
+++ components/forms/team/CreateTeamForm.tsx
@@ -65,7 +65,7 @@ export const CreateTeamForm: FC = () => {
         <button
           type="submit"
           disabled={isLoading}
-          className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:bg-gray-500 disabled:text-white"
         >
           생성하기
         </button>
