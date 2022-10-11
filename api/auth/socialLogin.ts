a376706451f7120c68fd5a146fe8c42d383da870
darae07 <leedr0730@gmail.com>
2022-10-11 11:09:07 +0900

    2,3.sdk로 로그인에 정보 제공 동의 scope 옵션 / 이메일 없을시 추가 동의 요청

--- api/auth/socialLogin.ts
+++ api/auth/socialLogin.ts
@@ -3,8 +3,11 @@ import { GoogleLoginResponse } from 'react-google-login'
 import { anonymousInstance } from '@/api/setupAxios'
 import { userLoginStart } from '@/store/modules'
 import { loginSuccess, loginFail } from './login'
+import { KAKAO_USERINFO_ERROR } from '@/constants/errorCode'
 
-export const kakaoLogin =
+const KAKAO_EMAIL_SCOPE = 'account_email'
+
+export const kakaoLoginCallback =
   (response: any) => async (dispatch: Dispatch<object>) => {
     try {
       dispatch(userLoginStart())
@@ -12,7 +15,7 @@ export const kakaoLogin =
         '/common/kakao-callback/',
         {
           headers: {
-            Authorization: response.response.access_token,
+            Authorization: response.access_token,
           },
         },
       )
@@ -22,9 +25,37 @@ export const kakaoLogin =
       } else {
         dispatch(loginFail(message || '카카오 인증에 실패했습니다.'))
       }
-    } catch {
-      dispatch(loginFail('카카오 인증에 실패했습니다.'))
+    } catch (err: any) {
+      const { result, message, success } = err.response.data
+      if (message == KAKAO_USERINFO_ERROR) {
+        dispatch(loginFail('이메일 제공 동의에 체크해 주세요.'))
+        dispatch(kakaoAuthorize(KAKAO_EMAIL_SCOPE))
+      } else {
+        dispatch(loginFail('카카오 인증에 실패했습니다.'))
+      }
     }
+  }
+
+export const kakaoAuthorize =
+  (scope?: string) => (dispatch: Dispatch<object>) => {
+    const config: any = {
+      success: function (response: any) {
+        dispatch(kakaoLoginCallback(response))
+      },
+      fail: function (error: any) {
+        if (typeof error === 'string') {
+          dispatch(loginFail(error))
+        } else if ('error_description' in error) {
+          dispatch(loginFail(error.error_description))
+        } else {
+          dispatch(loginFail('로그인에 실패했습니다.'))
+        }
+      },
+    }
+    if (scope) {
+      config.scope = scope
+    }
+    window.Kakao.Auth.login(config)
   }
 
 export const googleLogin =
